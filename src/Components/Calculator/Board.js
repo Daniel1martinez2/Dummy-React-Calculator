3acc45b9353ea5cda388139707a526364ad280c3
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-06-21 19:39:42 -0500

    spare code
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/Components/Calculator/Board.js
+++ src/Components/Calculator/Board.js
@@ -2,22 +2,10 @@ import classes from './Board.module.css';
 import Button from './Button/Button';
 
 const Board = props => {
+  const buttonsValue = ['7','8','9','*','4','5','6','-','1','2','3','+','0','.']; 
   return(
     <main className={classes.board}>
-      <Button onClick={props.onSetScreenValue}>7</Button>      
-      <Button onClick={props.onSetScreenValue}>8</Button>      
-      <Button onClick={props.onSetScreenValue}>9</Button>      
-      <Button onClick={props.onSetScreenValue}>*</Button>      
-      <Button onClick={props.onSetScreenValue}>4</Button>      
-      <Button onClick={props.onSetScreenValue}>5</Button>      
-      <Button onClick={props.onSetScreenValue}>6</Button>      
-      <Button onClick={props.onSetScreenValue}>-</Button>      
-      <Button onClick={props.onSetScreenValue}>1</Button>      
-      <Button onClick={props.onSetScreenValue}>2</Button>      
-      <Button onClick={props.onSetScreenValue}>3</Button>      
-      <Button onClick={props.onSetScreenValue}>+</Button>      
-      <Button onClick={props.onSetScreenValue}>0</Button>      
-      <Button onClick={props.onSetScreenValue}>.</Button>      
+      {buttonsValue.map(btn =><Button onClick={props.onSetScreenValue}>{btn}</Button>)}
       <Button onClick={()=>props.onSetResult()}>=</Button>      
     </main>
   ); 
